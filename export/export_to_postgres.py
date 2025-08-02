5994f63910dc246d90d67f6763f6dc23c3b9d842
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-08-02 18:32:37 -0400

    feat(export): add file so we can export data to postgres

---
+++ export/export_to_postgres.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import duckdb
+
+load_dotenv()
+
+PG_HOST = os.getenv("PG_HOST")
+PG_PORT = os.getenv("PG_PORT")
+PG_DB = os.getenv("PG_DB")
+PG_USER = os.getenv("PG_USER")
+PG_PASSWORD = os.getenv("PG_PASSWORD")
+PG_SCHEMA = os.getenv("PG_SCHEMA")
+
+def export_duckdb_to_postgres():
+    con = duckdb.connect("data/warehouse/sales.duckdb")
+    df = con.execute("SELECT * FROM sales").df()
+    con.close()
+
+    engine = create_engine(f"postgresql+psycopg2://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}")
+    with engine.begin() as conn:
+        result = conn.execute(text("""
+            SELECT schema_name FROM information_schema.schemata WHERE schema_name = :schema
+        """), {"schema": PG_SCHEMA})
+        if result.fetchone() is None:
+            conn.execute(text(f'CREATE SCHEMA "{PG_SCHEMA}"'))
+            print(f"🛠️  Schema '{PG_SCHEMA}' created")
+
+        conn.execute(text(f'SET search_path TO "{PG_SCHEMA}"'))
+
+        df.to_sql("sales", con=conn, index=False, if_exists="append", schema=PG_SCHEMA)
+        print("✅ Exported DuckDB sales table to PostgreSQL")
+
+if __name__ == "__main__":
+    export_duckdb_to_postgres()
