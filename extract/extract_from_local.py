5d25df3d43679bb40ff1a3e857b77e0098766d5a
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-08-05 16:01:08 -0400

    feat(extract): create copy of the raw data

--- extract/extract_from_local.py
+++ extract/extract_from_local.py
@@ -6,24 +6,29 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 
 SOURCE_DIR = "data/raw"
 DEST_DIR = "data/extract"
-FILENAME_PREFIX = "raw_sales_"
-COPY_PREFIX = "copy_raw_sales_"
+COPY_PREFIX = "copy_"
 
 def main():
     os.makedirs(DEST_DIR, exist_ok=True)
 
-    files = sorted([f for f in os.listdir(SOURCE_DIR) if f.startswith(FILENAME_PREFIX)], reverse=True)
-    if not files:
-        logging.error("No raw sales file found.")
+    csv_files = [f for f in os.listdir(SOURCE_DIR) if f.endswith(".csv")]
+
+    if not csv_files:
+        logging.error("❌ There's no .csv file in the origin folder.")
         return
 
-    latest_file = files[0]
-    file_number = latest_file.replace(FILENAME_PREFIX, "").replace(".csv", "")
-    src_path = os.path.join(SOURCE_DIR, latest_file)
-    dest_path = os.path.join(DEST_DIR, f"{COPY_PREFIX}{file_number}.csv")
+    csv_files_full_paths = [os.path.join(SOURCE_DIR, f) for f in csv_files]
+    csv_files_sorted = sorted(csv_files_full_paths, key=os.path.getmtime, reverse=True)
 
-    shutil.copy2(src_path, dest_path)
-    logging.info(f"Copied {src_path} to {dest_path}")
+    latest_file_path = csv_files_sorted[0]
+    latest_file_name = os.path.basename(latest_file_path)
+    logging.info(f"✅ Last file found: {latest_file_name}")
+
+    dest_file_name = COPY_PREFIX + latest_file_name.replace(".csv", "") + ".csv"
+    dest_path = os.path.join(DEST_DIR, dest_file_name)
+
+    shutil.copy2(latest_file_path, dest_path)
+    logging.info(f"📁 Copied file: {dest_path}")
 
 if __name__ == "__main__":
     main()
