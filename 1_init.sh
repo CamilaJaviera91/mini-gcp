4cd128a7d49bb4d49419dc06ec1701baabc8b883
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-08-10 20:40:36 -0400

    feat(bash): change docker compose, so we can initializate without errors

--- 1_init.sh
+++ 1_init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "🚀 Initializing Airflow..."
+
+DOCKER_BUILDKIT=0 docker compose build airflow-init
+
 docker compose run --rm airflow-init
 
 # 1. Make sure to give it permissions: chmod +x 1_init.sh
