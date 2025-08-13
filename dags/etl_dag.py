f731af688e5df9ad1bf7afc6887d7b98f387dc11
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-08-13 18:57:51 -0400

    feat(dag): add new task to the dag

--- dags/etl_dag.py
+++ dags/etl_dag.py
@@ -10,7 +10,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from scripts.generate_fake_data import generate_sales_data as sales
 from export.export_to_postgres import export_duckdb_to_postgres as postgres
 from initial_validation.initial_validation import validate_sales_data as validate
-from transform.transform_data_beam import run as transform
+from transform.transform_data_beam import transform_data_beam as transform
+from load.load_to_duckdb import load_to_duckdb as load
 
 default_args = {
     'owner': 'CamilaJaviera',
@@ -43,8 +44,13 @@ with DAG(
     )
 
     transform_task = PythonOperator(
-        task_id="run",
+        task_id="transform_data_beam",
         python_callable=transform
     )
 
-    sales_task >> postgres_task >> validate_task >> transform_task
+    load_task = PythonOperator(
+        task_id="load_to_duckdb",
+        python_callable=load
+    )
+
+    sales_task >> postgres_task >> validate_task >> transform_task >> load_task
