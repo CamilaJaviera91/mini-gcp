a2d25ad53b8137f9f80df5d5404dcbc0523549f8
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-08-13 18:56:57 -0400

    feat(load): change function name

--- load/load_to_duckdb.py
+++ load/load_to_duckdb.py
@@ -5,13 +5,13 @@ import glob
 DUCKDB_DIR = "data/warehouse"
 os.makedirs(DUCKDB_DIR, exist_ok=True)
 
-def main():
+def load_to_duckdb():
     db_path = os.path.join(DUCKDB_DIR, "sales.duckdb")
     con = duckdb.connect(db_path)
 
     csv_files = sorted(glob.glob("data/processed/clean_sales_*.csv"))
     if not csv_files:
-        print("❌ There's no clean_sales_*.csv in data/processed/")
+        print("❌ No hay archivos clean_sales_*.csv en data/processed/")
         return
 
     latest_file = csv_files[-1]
@@ -21,7 +21,7 @@ def main():
         SELECT * FROM read_csv_auto('{latest_file}', header=True)
     """)
 
-    print(f"✅ Last file: {latest_file}")
+    print(f"✅ Último archivo cargado: {latest_file}")
 
     df = con.execute("SELECT * FROM sales").df()
     
@@ -30,4 +30,4 @@ def main():
     con.close()
 
 if __name__ == "__main__":
-    main()
+    load_to_duckdb()
